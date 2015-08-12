561cfbe2d2afee8bb49bc3c37aa8b0869ef9a67e
Trevor Ewen <tewen@neosavvy.com>
2015-08-12 17:42:56 -0400

    Webdriver schedule version.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var Content = require('./scrapers/content');
 var Footer = require('./scrapers/footer');
 var Schedule = require('./scrapers/schedule-linear');
+var ScheduleWd = require('./scrapers/schedule-wd');
 
-Content().then(Footer);
-
+//Content().then(Footer);
+//
 //Schedule();
+
+ScheduleWd();
