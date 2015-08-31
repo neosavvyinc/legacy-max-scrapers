efa4e890615b5397ec45373b400c9acfb83e7b73
Sushindhran Harikrishnan <sushindhran@gmail.com>
2015-08-31 17:59:06 -0400

    Scraping complete data for cinemax schedule

--- scrapers/schedule-linear.js
+++ scrapers/schedule-linear.js
@@ -40,8 +40,6 @@ var _createDriver = function() {
 
 module.exports = function () {
     var BASE_DOMAIN = 'http://www.cinemax.com/apps/schedule/ScheduleServlet?ACTION_TODAY=TODAY';
-    var NEXT_GRID = 'http://www.cinemax.com/apps/schedule/ScheduleServlet?ACTION_NEXT_GRID=NEXT';
-    var PREV_GRID = 'http://www.cinemax.com/apps/schedule/ScheduleServlet?ACTION_PREV_GRID=PREV';
 
     return when.promise(function (resolve) {
         payload = {
@@ -84,6 +82,22 @@ var _getPrevPage = function(driver) {
     });
 };
 
+var _getNextPage = function(driver) {
+    return when.promise(function(resolve) {
+        setTimeout(function () {
+            driver.findElement(By.css('.schedule-wrapper'))
+                    .findElements(By.tagName('div'))
+                    .then(function(divs) {
+                        divs[0].findElements(By.tagName('a'))
+                                .then(function(a) {
+                                    a[1].click();
+                                    resolve(driver);
+                                });
+                    });
+        }, 2000);
+    });
+};
+
 //Get details for each program(by focusId)
 var _getProgramDetails = function(detailUrl) {
     var focusId = detailUrl.split('FOCUS_ID=')[1];
@@ -98,78 +112,83 @@ var _getProgramDetails = function(detailUrl) {
             driver.findElement(By.css('.schedule-wrapper'))
                     .findElement(By.tagName('div'))
                     .findElements(By.tagName('div'))
-                    .then(function(divs) {
-                        _getText(divs).then(function(textArr) {
-                            var content = _parseContent(textArr[1]);
-                            var genre = textArr[3].split(':');
-                            var channelInfo = _parseChannelInfo(textArr[10]);
+                    .then(function (divs) {
+                        try {
+                            _getText(divs).then(function (textArr) {
+                                var content = _parseContent(textArr[1]);
+                                var genre = textArr[3].split(':');
+                                var channelInfo = _parseChannelInfo(textArr[10]) || [];
 
-                            _.assign(_.get(details, 'response.body.ProductResponse.productSummary'), {
-                                CMSSubCategories: 'movies',
-                                blurbId: '',
-                                castCrew: _getCastCrew(textArr[8], textArr[9]),
-                                title: textArr[0],
-                                content: content,
-                                defaultCategory: {category: 'Movies'},
-                                detailImg: "http://i.lv3.hbo.com/assets/images/documentaries/seduced-and-abandoned/seduced-and-abandoned-380.jpg",
-                                focusId: focusId,
-                                genre: genre,
-                                isHD: "Y",
-                                landingLink: "",
-                                mpaa: "R",
-                                pid: 0,
-                                runTimeMins: textArr[2].split(': ')[1],
-                                runTimeSecs: 0,
-                                summary: _.trim(textArr[7], '"'),
-                                vid: 0
-                            });
-
-                            _.assign(programs, {
-                                title: textArr[0],
-                                focusId: focusId,
-                                productId: 'p' + focusId
-                            });
-
-                            channelInfo.map(function(channel) {
-                                _.get(programs, 'LinearSchedule').push({
-                                    AirDate: _parseToISODate(channel[0], channel[1]),
-                                    channel: channel[2].replace(' -', '')
+                                _.assign(_.get(details, 'response.body.ProductResponse.productSummary'), {
+                                    CMSSubCategories: 'movies',
+                                    blurbId: '',
+                                    castCrew: _getCastCrew(textArr[8], textArr[9]),
+                                    title: textArr[0],
+                                    content: content,
+                                    defaultCategory: {category: 'Movies'},
+                                    detailImg: "http://i.lv3.hbo.com/assets/images/documentaries/seduced-and-abandoned/seduced-and-abandoned-380.jpg",
+                                    focusId: focusId,
+                                    genre: genre,
+                                    isHD: "Y",
+                                    landingLink: "",
+                                    mpaa: "R",
+                                    pid: 0,
+                                    runTimeMins: textArr[2].split(': ')[1],
+                                    runTimeSecs: 0,
+                                    summary: _.trim(textArr[7], '"'),
+                                    vid: 0
                                 });
-                            });
 
-                            _.get(payload, 'schedule.miniSummary').push({
-                                '@focusId': focusId,
-                                PID: 0,
-                                content: content,
-                                defaultCategory: {category: 'Movies'},
-                                genre: genre,
-                                mpaa: "R",
-                                seasonYear: 0,
-                                summary: _.trim(textArr[7], '"'),
-                                title: textArr[0]
-                            });
+                                _.assign(programs, {
+                                    title: textArr[0],
+                                    focusId: focusId,
+                                    productId: 'p' + focusId
+                                });
 
-                            _.get(payload, 'schedule.linearSchedule').push({
-                                channelId: 28, //This is not correct, but think it doesn't matter.
-                                channelName: channelInfo[0][2].replace(' -', ''),
-                                duration: textArr[2].split(': ')[1],
-                                focusId: focusId,
-                                isHD: "Y",
-                                isHboGOPlay: "Y",
-                                isOnDemandPlay: "Y",
-                                nextPlayDate: '',
-                                pid: 0, //irrelevant for cinemax
-                                playDate: _parseToISODate(channelInfo[0][0], channelInfo[0][1]),
-                                vid: 0
-                            });
+                                channelInfo.map(function (channel) {
+                                    _.get(programs, 'LinearSchedule').push({
+                                        AirDate: _parseToISODate(channel[0], channel[1]),
+                                        channel: channel[2].replace(' -', '')
+                                    });
+                                });
 
-                            setTimeout(function () {
-                                driver.quit();
-                            }, 2000);
-                            fs.writeFileSync('./out/'+focusId+'.json', JSON.stringify(details));
-                            fs.writeFileSync('./out/p'+focusId+'.json', JSON.stringify(programs));
+                                _.get(payload, 'schedule.miniSummary').push({
+                                    '@focusId': focusId,
+                                    PID: 0,
+                                    content: content,
+                                    defaultCategory: {category: 'Movies'},
+                                    genre: genre,
+                                    mpaa: "R",
+                                    seasonYear: 0,
+                                    summary: _.trim(textArr[7], '"'),
+                                    title: textArr[0]
+                                });
+
+                                _.get(payload, 'schedule.linearSchedule').push({
+                                    channelId: 28, //This is not correct, but think it doesn't matter.
+                                    channelName: channelInfo[0][2].replace(' -', ''),
+                                    duration: textArr[2].split(': ')[1],
+                                    focusId: focusId,
+                                    isHD: "Y",
+                                    isHboGOPlay: "Y",
+                                    isOnDemandPlay: "Y",
+                                    nextPlayDate: '',
+                                    pid: 0, //irrelevant for cinemax
+                                    playDate: _parseToISODate(channelInfo[0][0], channelInfo[0][1]),
+                                    vid: 0
+                                });
+
+                                setTimeout(function () {
+                                    driver.quit();
+                                }, 2000);
+                                fs.writeFileSync('./out/' + focusId + '.json', JSON.stringify(details));
+                                fs.writeFileSync('./out/p' + focusId + '.json', JSON.stringify(programs));
+                                resolve();
+                            });
+                        } catch(e) {
+                            console.log(e);
                             resolve();
-                        });
+                        }
                     });
         }, 2000);
     });
@@ -185,34 +204,48 @@ var _getAllPrograms = function(url) {
 
         //For first page
         allPrograms.push(_getAllProgramsOnPage(driver));
-        //For previous 10 links
-        resolve(_getAllPrevLinks(allPrograms, driver, 0));
+        //One next link
+        _getOneNextLink(allPrograms, driver).then(function(programs) {
+            //For previous 10 links
+            resolve(_getAllPrevLinks(programs, driver, 0));
+        });
     });
 };
 
 //For previous 10 links
 var _getAllPrevLinks = function(allPrograms, driver, count) {
     return when.promise(function(resolve) {
-        if(count >= 9) {
-            when.all(allPrograms).then(function(links) {
-                setTimeout(function () {
-                    driver.quit();
-                }, 2000);
-                console.log('Unique', _.uniq(_.flatten(links)).length);
-                resolve(_.uniq(_.flatten(links)));
-            });
-        } else {
-            _getPrevPage(driver).then(function (driver) {
-                allPrograms.push(_getAllProgramsOnPage(driver));
-                _getAllPrevLinks(allPrograms, driver, count + 1).then(function(programs) {
-                    return when.promise(function(res) {
-                        setTimeout(function() {
+        setTimeout(function() {
+            if(count >= 9) {
+                when.all(allPrograms).then(function(links) {
+                    setTimeout(function () {
+                        driver.quit();
+                    }, 2000);
+                    console.log('Unique', _.uniq(_.flatten(links)).length);
+                    resolve(_.uniq(_.flatten(links)));
+                });
+            } else {
+                _getPrevPage(driver).then(function (driver) {
+                    allPrograms.push(_getAllProgramsOnPage(driver));
+                    _getAllPrevLinks(allPrograms, driver, count + 1).then(function(programs) {
+                        return when.promise(function(res) {
                             resolve(programs);
-                        }, 2000);
+                        });
                     });
                 });
+            }
+        }, 2000);
+    });
+};
+
+var _getOneNextLink = function(allPrograms, driver) {
+    return when.promise(function(resolve) {
+        setTimeout(function() {
+            _getNextPage(driver).then(function(driver) {
+                allPrograms.push(_getAllProgramsOnPage(driver));
+                resolve(allPrograms);
             });
-        }
+        }, 2000);
     });
 };
 
@@ -307,10 +340,4 @@ var _parseToISODate = function(date, time) {
     var d = '2015/' + date.split(' ')[1];
     var t = time.split(' ')[0] + ':00';
     return new Date(d + ' ' + t).toISOString().replace('.000Z', 'Z');
-};
-
-var _getAttribute = function(element, attrib) {
-    element.getAttribute(attrib).then(function(value) {
-        return value;
-    });
 };
